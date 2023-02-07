73436f2627ab30d609e18e27ea360dbaee808c74
JadeGJ <50872761+JadeGJ@users.noreply.github.com>
2023-02-07 18:42:56 +0000

    Initial Commit

---
+++ code history/code_historyApp.swift
@@ -0,0 +1,17 @@
+//
+//  code_historyApp.swift
+//  code history
+//
+//  Created by Jade Gordon-Johnson on 07/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct code_historyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
